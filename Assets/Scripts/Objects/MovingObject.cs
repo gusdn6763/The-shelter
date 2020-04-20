8a68718df17acef4adff9cc40c49cbbc9dd1017f
hyejang <hyejang@student.42seoul.kr>
2020-04-20 20:28:18 +0900

    Joy_stick & player move
    2020-04-20 7:30 조이스틱과 플레이어 움직임 구현 및 애니매이션, 연동 구현

---
+++ Assets/Scripts/Objects/MovingObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(CapsuleCollider2D))]
+public class MovingObject : MonoBehaviour
+{
+    protected Animator animator;
+
+    protected CapsuleCollider2D dmgCheck;
+
+    public GameObject target;
+
+    public float FullHp;
+    public float currentHp;
+    public float speed;
+    public float attack_Delay;
+    public float attack_Delay_Tmp;
+    public float add_Damage;
+
+    public virtual void Awake()
+    {
+        animator = GetComponent<Animator>();
+        dmgCheck = GetComponent<CapsuleCollider2D>();
+
+        FullHp = 100;
+        speed = 10f;
+        attack_Delay = 1f;
+        add_Damage = 10f;
+    }
+
+    public void ShowTarget()
+    {
+        Vector3 dir = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public void Damaged(float damage)
+    {
+        currentHp -= damage;
+        if (currentHp<=0)
+        {
+            Die();
+        } 
+    }
+
+    public virtual void Die()
+    {
+        dmgCheck.enabled = false;
+        Debug.Log(this.gameObject.name+"죽음");
+    }
+}
---
+++ Assets/Scripts/Objects/Player/PlayerManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MovingObject
+{
+    public enum CharacterStatus
+    {
+        NONE,
+        IDLE,
+        MOVE,
+        ATTACK,
+        DIE
+    }
+
+    public CharacterStatus playerStatus;                /// joystick 스크립트로 변경
+
+    public override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void Update()
+    {
+        switch (playerStatus)
+        {
+            case CharacterStatus.NONE:
+                break;
+            case CharacterStatus.IDLE:
+                    attack_Delay_Tmp = attack_Delay;
+                    playerStatus = CharacterStatus.ATTACK;
+                animator.SetBool("Move", false);
+                break;
+            case CharacterStatus.MOVE:
+                transform.Translate((Vector3.down * (speed * 0.1f)) * Time.deltaTime);
+                animator.SetBool("Move", true);
+                break;
+            case CharacterStatus.ATTACK:
+                ShowTarget();
+                if (attack_Delay >= 0)
+                    attack_Delay_Tmp -= Time.deltaTime;
+                else
+                {
+                    attack_Delay_Tmp = attack_Delay;
+                    animator.SetTrigger("Attack_Rifle");
+                }
+                break;
+            case CharacterStatus.DIE:
+                break;
+        }
+    }
+
+    //나중에 처음 시작할때 자동이로 움직여주는 함수
+    public void Move(float distance,float startTime)        
+    {
+        StartCoroutine(MoveCoroutine(distance, startTime));
+    }
+
+    IEnumerator MoveCoroutine(float distance,float startTime)
+    {
+        Vector3 toPos = new Vector3(transform.position.x, transform.position.y+distance, transform.position.z);
+        while (startTime > 0)
+        {
+            startTime -= Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, toPos,(startTime+(Time.deltaTime/startTime))-startTime);
+            yield return null;
+        }
+        playerStatus = CharacterStatus.IDLE;
+    }
+}
