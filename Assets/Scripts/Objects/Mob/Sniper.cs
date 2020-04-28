6034e37758a243a71526458a2d7deafd354b9965
hyejang <hyejang@student.42seoul.kr>
2020-04-28 17:40:38 +0900

    Revert "Revert "sniper fix, trash code delete""
    This reverts commit a84a3a996acbf2ce804224adc63c9d34b6dedc45.

--- Assets/Scripts/Objects/Mob/Sniper.cs
+++ Assets/Scripts/Objects/Mob/Sniper.cs
@@ -14,36 +14,15 @@ public class Sniper : Mob
         while (!isDie)
         {
             if (enemyStatus == CharacterStatus.DIE) yield break;
-            if (FindPlayer())
+
+            if (fireCtrl.isReload)
             {
-                if (CollEtcObject())
-                {
-                    if (Comparison())
-                    {
-                        enemyStatus = CharacterStatus.ATTACK;
-                    }
-                    else
-                    {
-                        enemyStatus = CharacterStatus.TRACE;
-                    }
-                }
-                else
-                {
-                    enemyStatus = CharacterStatus.ATTACK;
-                }
+                enemyStatus = CharacterStatus.AVODING;
             }
             else
             {
-                if (CollEtcObject())
-                {
-                    enemyStatus = CharacterStatus.FAR_TRACE;
-                }
-                else
-                {
-                    enemyStatus = CharacterStatus.MOVE;
-                }
+                enemyStatus = CharacterStatus.ATTACK;
             }
-            yield return null;
         }
     }
     public override IEnumerator StartAction()
@@ -55,21 +34,17 @@ public class Sniper : Mob
                 case CharacterStatus.NONE:
                     break;
                 case CharacterStatus.IDLE:
-                    animator.SetBool("Move", false);
                     break;
                 case CharacterStatus.MOVE:
-                    ShowTarget();
-                    transform.Translate((Vector3.down * (speed * 0.1f)) * Time.deltaTime);
+                    break;
+                case CharacterStatus.AVODING:
+                    Avoding();
+                    transform.Translate((Vector3.down * (speed * 0.05f)) * Time.deltaTime);
                     animator.SetBool("Move", true);
                     break;
                 case CharacterStatus.TRACE:
-                    animator.SetBool("Move", true);
-                    agent.SetDestination(target.transform.position);
-
                     break;
                 case CharacterStatus.FAR_TRACE:
-                    animator.SetBool("Move", true);
-                    agent.SetDestination(target.transform.position);
                     break;
                 case CharacterStatus.ATTACK:
                     animator.SetBool("Move", false);
@@ -78,10 +53,10 @@ public class Sniper : Mob
                     fireCtrl.isFire = true;
                     if (!fireCtrl.isReload && fireCtrl.isFire)
                     {
-                        if (Time.time >= fireCtrl.nextFire)        //현재 시간이 다음 발사 시간보다 큰지를 확인
+                        if (Time.time >= fireCtrl.nextFire)
                         {
                             animator.SetTrigger("Attack");
-                            fireCtrl.nextFire = Time.time + fireCtrl.fireRate + Random.Range(0.0f, 0.3f); //다음 발사 시간 계산
+                            fireCtrl.nextFire = Time.time + fireCtrl.fireRate + Random.Range(0.0f, 0.3f);
                         }
                     }
                     break;
