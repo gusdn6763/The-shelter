6c43549231c719d39281c1221dad49d07da64c88
PremiumCube <30944231+PremiumCube@users.noreply.github.com>
2020-05-02 21:16:52 +0900

    수정: 카메라 사이즈 수정

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -36,7 +36,7 @@ public class CameraManager : MonoBehaviour
             int mapCol = MapManager.instance.maps[GameManager.instance.player.nowStage].mapInfo.mapColumns;
             targetPosition.Set(target.transform.position.x, target.transform.position.y, - 10f);
             targetPosition.x = Mathf.Clamp(target.transform.position.x, mapPosition.x + mapRow / 2 - 3 + 3.5f, mapPosition.x - mapRow / 2 + 3 + 3.5f); // 임의의 값, 3.5f는 Map이 -3.5f 좌표값을 갖기때문에 더해줌
-            targetPosition.y = Mathf.Clamp(target.transform.position.y, mapPosition.y, mapPosition.y + mapCol / 2 + (mapCol % 2) - 5); // 임의의 값
+            targetPosition.y = Mathf.Clamp(target.transform.position.y, mapPosition.y, mapPosition.y + mapCol - 10); // 10 카메라 길이
             trans.position = Vector3.Lerp(trans.position, targetPosition, moveSpeed * Time.deltaTime);
         }
     }
